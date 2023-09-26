762f63d813dfbf958b09e6b36f7147ddb5fa8aa5
Pham Gi <trungdunght2003@gmail.com>
2023-09-26 11:29:01 +0700

    Intent to SecondActivity

--- app/src/main/java/com/example/cn1403/MainActivity.java
+++ app/src/main/java/com/example/cn1403/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cn1403;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,19 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); // Show the back button
+            actionBar.setTitle("Danh sach sinh vien"); // Set the title
+        }
+
         Button bt_1 = (Button) findViewById(R.id.bt_1);
         bt_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText et_1 = (EditText) findViewById(R.id.et_1);
-                TextView tv_1 = (TextView) findViewById(R.id.tv_1);
-
-                String aaa = et_1.getText().toString();
-                tv_1.setText(aaa);
+                EditText et = (EditText) findViewById(R.id.et_1);
+                String aaa = et.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name_string", aaa);
+                startActivity(intent);
 
             }
         });
-
-
     }
 }
